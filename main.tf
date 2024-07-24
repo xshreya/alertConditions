47e9c44a6f64dc568337d1c3794e090e5cc1c90d
xshreya <shreyaa.shrmaa@gmail.com>
2024-07-24 16:31:06 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -13,17 +13,17 @@ resource "newrelic_nrql_alert_condition" "condition" {
   name                           = each.value.name
   description                    = each.value.description
   # runbook_url                    = "https://www.example.com"
-  enabled                        = true
-  violation_time_limit_seconds   = 3600
+  enabled                        = each.value.enabled
+  violation_time_limit_seconds   = each.value.violation_time_limit_seconds
   fill_option                    = each.value.fill_option_value
   # fill_value                     = 1.0
-  aggregation_window             = 60
-  aggregation_method             = "event_flow"
-  aggregation_delay              = 120
-  expiration_duration            = 120
-  open_violation_on_expiration   = true
-  close_violations_on_expiration = true
-  slide_by                       = 30
+  aggregation_window             = each.value.aggregation_window
+  aggregation_method             = each.value.aggregation_method
+  aggregation_delay              = each.value.aggregation_delay
+  expiration_duration            = each.value.expiration_duration
+  open_violation_on_expiration   = each.value.open_violation_on_expiration
+  close_violations_on_expiration = each.value.close_violations_on_expiration
+  slide_by                       = each.value.slide_by
 
   nrql {
     query = each.value.query
